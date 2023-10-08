73f3d66d2cbc452dc29e11d22421c6e14cde940a
Daniel Bahrami <daniel@bahrami.dk>
2023-10-08 21:56:41 +0200

    Read csv

--- src/emotion_analysis.py
+++ src/emotion_analysis.py
@@ -1,3 +1,10 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
+# Read CSV file into a DataFrame
+file_path = ''  # Update this with the actual path to your CSV file
+df = pd.read_csv(file_path, delimiter=',')
+
+# Convert the 'timestamp' column to datetime for proper plotting
+df['timestamp'] = pd.to_datetime(df['timestamp'])
+
