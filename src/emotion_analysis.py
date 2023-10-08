669f5c147754bd851c6dc754debc37ec89b0fd55
Daniel Bahrami <daniel@bahrami.dk>
2023-10-08 21:58:49 +0200

    Plot data

--- src/emotion_analysis.py
+++ src/emotion_analysis.py
@@ -8,3 +8,22 @@ df = pd.read_csv(file_path, delimiter=',')
 # Convert the 'timestamp' column to datetime for proper plotting
 df['timestamp'] = pd.to_datetime(df['timestamp'])
 
+# Plotting
+plt.figure(figsize=(10, 6))
+
+# Plot each emotion
+emotions = ['happiness', 'sadness', 'anger', 'fear', 'surprise', 'disgust']
+for emotion in emotions:
+    plt.plot(df['timestamp'], df[emotion], label=emotion)
+
+# Customize the plot
+plt.xlabel('Timestamp')
+plt.ylabel('Emotion Strength')
+plt.title('Emotion Changes Over Time')
+plt.legend()
+plt.grid(True)
+
+# Show the plot
+plt.tight_layout()
+plt.xticks(rotation=45)
+plt.show()
