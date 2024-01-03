c2638a7aa929cdc557e86e5ecf94cdfea21ea4e2
Daniel Bahrami <daniel@bahrami.dk>
2024-01-03 15:57:36 +0100

    Update emotion_analysis.py

--- src/emotion_analysis.py
+++ src/emotion_analysis.py
@@ -1,8 +1,8 @@
-import pandas as pd
 import matplotlib.pyplot as plt
+import pandas as pd
 
 # Read CSV file into a DataFrame
-file_path = '.\output\csv\emotions_data_2023-10-10_12-28-58.csv'  # Update this with the actual path to your CSV file
+file_path = ''  # Update this with the path to your CSV file
 df = pd.read_csv(file_path, delimiter=',')
 
 # Convert the 'timestamp' column to datetime for proper plotting
@@ -11,15 +11,15 @@ df['timestamp'] = pd.to_datetime(df['timestamp'])
 # Plotting
 plt.figure(figsize=(10, 6))
 
-# Plot each emotion
-emotions = ['happiness', 'sadness', 'anger', 'fear', 'surprise', 'disgust']
-for emotion in emotions:
+# Plot each negative emotion
+negative_emotions = ['sadness', 'anger', 'fear', 'disgust']
+for emotion in negative_emotions:
     plt.plot(df['timestamp'], df[emotion], label=emotion)
 
 # Customize the plot
 plt.xlabel('Timestamp')
 plt.ylabel('Emotion Strength')
-plt.title('Emotion Changes Over Time')
+plt.title('Negative Emotion Changes Over Time')
 plt.legend()
 plt.grid(True)
 
